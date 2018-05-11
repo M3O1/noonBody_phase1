75f56f9d8c3065148dfcff58efec766a88feceef
sangjaekang <sangjaekang48@gmail.com>
2018-05-11 18:56:28 +0900

    model parameter 변경가능하도록 수정

--- model/model.py
+++ model/model.py
@@ -6,26 +6,26 @@ from keras import models, backend, Model
 from keras.layers import Input, Conv2D, MaxPool2D, ZeroPadding2D
 from keras.layers import BatchNormalization, Flatten, Dense, Reshape
 
-def get_1000fps_model(input_size):
+def get_1000fps_model(input_size,filters=64):
     input_shape = (*input_size,3)
     x = Input(input_shape,name="input")
     #First Convolution Block
     block_name = "conv1-"
     inode = ZeroPadding2D(padding=(2,2),name=block_name+"padding")(x)
-    inode = Conv2D(64, (5,5), strides=(1,1), activation='relu',name=block_name+"conv")(inode)
+    inode = Conv2D(filters, (5,5), strides=(1,1), activation='relu',name=block_name+"conv")(inode)
     inode = MaxPool2D(pool_size=(3,3),strides=2,name=block_name+"pool")(inode)
     inode = BatchNormalization(name=block_name+"batchnorm")(inode)
 
     #Second Convolution Block
     block_name = "conv2-"
     inode = ZeroPadding2D(padding=(2,2),name=block_name+"padding")(inode)
-    inode = Conv2D(64, (5,5), strides=(1,1), activation='relu',name=block_name+"conv")(inode)
+    inode = Conv2D(filters, (5,5), strides=(1,1), activation='relu',name=block_name+"conv")(inode)
     inode = MaxPool2D(pool_size=(3,3),strides=2,name=block_name+"pool")(inode)
     inode = BatchNormalization(name=block_name+"batchnorm")(inode)
 
     #Third Convolution Block
     block_name = "conv3-"
-    inode = Conv2D(64, (3,3), strides=(1,1), activation='relu',name=block_name+"conv")(inode)
+    inode = Conv2D(filters, (3,3), strides=(1,1), activation='relu',name=block_name+"conv")(inode)
 
     #First FC layer
     block_name = "fc1"
@@ -38,7 +38,7 @@ def get_1000fps_model(input_size):
 
     #output layer
     block_name = "out-"
-    inode = Dense(48*48, activation='sigmoid',name=block_name+"fc")(inode)
+    inode = Dense(input_size[0]*input_size[1], activation='sigmoid',name=block_name+"fc")(inode)
 
-    y = Reshape((48,48),name=block_name+'reshape')(inode)
+    y = Reshape(input_size,name=block_name+'reshape')(inode)
     return Model(x,y)
